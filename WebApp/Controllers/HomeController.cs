7d885f402ff90e3434ec46708539e752db0bf627
mikolaj.babicz <mikolaj.babicz@microsoft.wsei.edu.pl>
2024-10-09 11:49:53 +0200

    comit2

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -28,6 +28,42 @@ public class HomeController : Controller
     
     public IActionResult Calculator()
     {
+
+        //https://localhost:7172/Home/Calculator?ap=add&x=4&y=1,5
+        var op=Request.Query["op"];
+        
+        var x=double.Parse(Request.Query["x"]);
+        
+        var y=double.Parse(Request.Query["y"]);
+
+        var result = 0.0d;
+
+        switch (op)
+        {
+            case "add":
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            
+            case "sub":
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            
+            case "mul":
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            
+            case "div":
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+        }
+        ViewBag.Resukt = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
+        //ViewBag.Operator = op;
         return View();
     }
     public IActionResult About()
